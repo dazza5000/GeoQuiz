f252fe1ac13208030616c9672755bdfbbf4c2502
Darran Kelinske <Darran.Kelinske@servicemesh.com>
2015-12-19 17:04:01 -0600

    Chapter4 complete

--- app/src/main/java/com/bignerdrach/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdrach/android/geoquiz/QuizActivity.java
@@ -1,11 +1,7 @@
 package com.bignerdrach.android.geoquiz;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -139,6 +135,7 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void updateQuestion(){
+//        Log.d(TAG, "Updating question text for question #" +mCurrentIndex, new Exception());
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
